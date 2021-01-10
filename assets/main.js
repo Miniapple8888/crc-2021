d9b177a65c1669733eb2981f294a91ecdee25c80
Miniapple8888 <miniapple8888@gmail.com>
2021-01-10 17:29:34 -0500

    removed comment

--- assets/main.js
+++ assets/main.js
@@ -10,11 +10,3 @@ $('.nav-item a').on('click', function() {
     $('.hamburger-toggler').removeClass('toggled');
     $('#navbar-collapse').removeClass('show');
 })
-/*
-$(".nav-item a").click(function(event) {
-    event.preventDefault();
-    console.log($(this).attr('href'));
-    $("html, body").animate({
-        scrollTop: $($(this).attr("href")).offset().top
-      }, 500);
-});*/
