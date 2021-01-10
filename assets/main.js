977bb0c3282766b98e35f0a107a1366843253cf2
Miniapple8888 <miniapple8888@gmail.com>
2021-01-10 16:28:00 -0500

    Added link animation+navbar toggle style

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
 $(window).scroll(function() {
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
 })
+$('.hamburger-toggler').on('click', function() {
+    $('nav').toggleClass('toggled');
+    $('.hamburger-toggler').toggleClass('toggled');
+})
