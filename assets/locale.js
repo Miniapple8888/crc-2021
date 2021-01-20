9766c341ec74660c250535ecaf9b52b0da5d18fc
Hircinus <j.alfahad@gmail.com>
2021-01-20 16:44:22 -0500

    Port locale settings to /fr/ folder

--- assets/locale.js
+++ assets/locale.js
@@ -1,7 +1,20 @@
-let hash = window.location.hash;
-document.getElementById("translate").innerHTML = '<a href="fr/accueil.html' + hash + '">French</a>';
-window.addEventListener('hashchange', handleClick);
-function handleClick() {
-    let hash = window.location.hash;
+var hash = window.location.hash;
+var filePath = window.location.pathname;
+var parts = filePath.split("/");
+var currentFolder = parts[parts.length - 2];
+if(currentFolder == "fr") {
+    document.getElementById("translate").innerHTML = '<a href="../index.html' + hash + '">English</a>';
+    window.addEventListener('hashchange', handleClick);
+    function handleClick() {
+        hash = window.location.hash;
+        document.getElementById("translate").innerHTML = '<a href="../index.html' + hash + '">English</a>';
+    }
+}
+else {
     document.getElementById("translate").innerHTML = '<a href="fr/accueil.html' + hash + '">French</a>';
+    window.addEventListener('hashchange', handleClick);
+    function handleClick() {
+        hash = window.location.hash;
+        document.getElementById("translate").innerHTML = '<a href="fr/accueil.html' + hash + '">French</a>';
+    }
 }
